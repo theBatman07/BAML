e1616d407f3def3164f0220a5a41e4d58a60c701
Gaurav Khater <85270515+theBatman07@users.noreply.github.com>
2025-05-14 11:18:03 +0530

    update env

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ from typing import List, Dict
 from baml_client import b
 from baml_client.types import Tool, Instruction
 
+from dotenv import load_dotenv
+load_dotenv() 
+
 app = FastAPI()
 
 class Tool(BaseModel):
@@ -11,9 +14,9 @@ class Tool(BaseModel):
     description: str 
     parameters: Dict[str, str]
 
-class ToolCall(BaseModel):
-    tool_name: str
-    args: Dict[str, any]
+# class ToolCall(BaseModel):
+#     tool_name: str
+#     args: Dict[str, any]
 
 class PlannerRequest(BaseModel):
     instructions: str
@@ -25,4 +28,9 @@ class PlannerResponse(BaseModel):
 
 @app.post("/plannar")
 async def generate_plans(request: PlannerRequest) -> PlannerResponse:
-    pass
+    instruction = Instruction(description=request.instruction)
+    tools = [Tool(name=tool) for tool in request.tools]
+
+    response = await b.GeneratePlans(instruction, tools)
+
+    return PlannerResponse(plan=response.plans)
